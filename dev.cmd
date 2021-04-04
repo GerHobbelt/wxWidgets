175eb567cd75b9109f691b7487d186237b3066f2
yurrig <35352239+yurrig@users.noreply.github.com>
2021-04-04 00:22:49 -0500

    Paths changes reflecting new deps location

--- dev.cmd
+++ dev.cmd
@@ -1,10 +1,10 @@
-@echo off
+rem @echo off
 set "PF=%ProgramFiles(x86)%"
 if "%PF%"=="" set "PF=%ProgramFiles%"
 call "%PF%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 set VC="%VCIDEInstallDir%..\devenv.exe"
 set PreferredToolArchitecture=x64
-set DEPS=d:\oss
-set BOOST=boost_1_75_0
-set LIB=%LIB%;d:\oss\lib
+set DEPS=%~dp0deps
+if exist "\local_settings.cmd" call "\local_settings.cmd"
+set LIB=%LIB%;%DEPS%\lib
 start "" %VC% "%~dp0pcb.sln"
