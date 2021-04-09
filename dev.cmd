ad2cac65b10949ef9c255dc89de40d6dfbb0344d
yurrig <35352239+yurrig@users.noreply.github.com>
2021-04-08 20:01:12 -0500

    Update paths to dependencies

--- dev.cmd
+++ dev.cmd
@@ -6,7 +6,8 @@ set VC="%VCIDEInstallDir%..\devenv.exe"
 set PreferredToolArchitecture=x64
 set DEPS=%~dp0deps
 if exist \local_settings.cmd call \local_settings.cmd
-set LIB=%LIB%;%DEPS%\lib
+set LIBDBG=%DEPS%\blend2d\out\build\x64-Debug;%BOOST%\stage\lib;%LIB%
+set LIBREL=%DEPS%\blend2d\out\build\x64-Release;%BOOST%\stage\lib;%LIB%
 set PYTHON=%LOCALAPPDATA%\Programs\Python\Python38\python.exe
 set CLANG_FORMAT="%ProgramW6432%\llvm\bin\clang-format.exe"
 start "" %VC% "%~dp0pcb.sln"
