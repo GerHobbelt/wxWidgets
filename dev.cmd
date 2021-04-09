a9b603daf0f3970c2257c750830c8ce28eae316c
yurrig <35352239+yurrig@users.noreply.github.com>
2021-04-08 20:13:06 -0500

    Correct dxflib

--- dev.cmd
+++ dev.cmd
@@ -6,8 +6,8 @@ set VC="%VCIDEInstallDir%..\devenv.exe"
 set PreferredToolArchitecture=x64
 set DEPS=%~dp0deps
 if exist \local_settings.cmd call \local_settings.cmd
-set LIBDBG=%DEPS%\blend2d\out\build\x64-Debug;%BOOST%\stage\lib;%LIB%
-set LIBREL=%DEPS%\blend2d\out\build\x64-Release;%BOOST%\stage\lib;%LIB%
+set LIBDBG=%DEPS%\blend2d\out\build\x64-Debug;%DEPS%\dxflib\debug;%BOOST%\stage\lib;%LIB%
+set LIBREL=%DEPS%\blend2d\out\build\x64-Release;%DEPS%\dxflib\release;%BOOST%\stage\lib;%LIB%
 set PYTHON=%LOCALAPPDATA%\Programs\Python\Python38\python.exe
 set CLANG_FORMAT="%ProgramW6432%\llvm\bin\clang-format.exe"
 start "" %VC% "%~dp0pcb.sln"
