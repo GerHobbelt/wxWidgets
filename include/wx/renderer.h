6d5fd6e14665212dfdb3e1e23420458652cc0038
redtide <redtid3@gmail.com>
2021-07-01 17:35:56 +0200

    Added DrawTab() to wxRendererNative

--- include/wx/renderer.h
+++ include/wx/renderer.h
@@ -347,6 +347,14 @@ public:
                                     int flags = 0) = 0;
 #endif // wxHAS_DRAW_TITLE_BAR_BITMAP
 
+    virtual void DrawTab(wxDC& dc,
+                         const wxRect& rect,
+                         wxDirection direction,
+                         const wxString& label,
+                         const wxBitmap& bitmap = wxNullBitmap,
+                         int flags = 0,
+                         int indexAccel = -1) = 0;
+
     // Draw a gauge with native style like a wxGauge would display.
     //
     // wxCONTROL_SPECIAL flag must be used for drawing vertical gauges.
@@ -564,6 +572,15 @@ public:
                                     int flags = 0) wxOVERRIDE
         { m_rendererNative.DrawTitleBarBitmap(win, dc, rect, button, flags); }
 #endif // wxHAS_DRAW_TITLE_BAR_BITMAP
+
+    virtual void DrawTab(wxDC& dc,
+                         const wxRect& rect,
+                         wxDirection dir,
+                         const wxString& label,
+                         const wxBitmap& bitmap = wxNullBitmap,
+                         int flags = 0,
+                         int indexAccel = -1) wxOVERRIDE
+        { m_rendererNative.DrawTab(dc, rect, dir, label, bitmap, flags, indexAccel); }
 
     virtual void DrawGauge(wxWindow* win,
                            wxDC& dc,
