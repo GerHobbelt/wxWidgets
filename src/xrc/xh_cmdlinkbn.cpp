6101b18ca6d60a2e1fdf092009237f74cf46da2e
Alexander Koshelev <akoshelev@tt-solutions.com>
2021-12-31 14:08:56 +0300

    XRC: ability to create wxCommandLinkButton with wxBitmapBundle

--- src/xrc/xh_cmdlinkbn.cpp
+++ src/xrc/xh_cmdlinkbn.cpp
@@ -46,7 +46,12 @@ wxObject *wxCommandLinkButtonXmlHandler::DoCreateResource()
     if (GetBool(wxT("default"), 0))
         button->SetDefault();
 
-    if ( GetParamNode("bitmap") )
+    if ( HasParam("bitmaps") )
+    {
+        button->SetBitmap(GetBitmapBundle("bitmaps", wxART_BUTTON),
+                          GetDirection("bitmapposition"));
+    }
+    else if ( GetParamNode("bitmap") )
     {
         button->SetBitmap(GetBitmap("bitmap", wxART_BUTTON),
                           GetDirection("bitmapposition"));
