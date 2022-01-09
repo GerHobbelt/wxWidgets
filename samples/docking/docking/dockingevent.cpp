93974e7ceb4745f60b03562dc691462ba44db726
Gerhard Gruber <sparhawk@gmx.at>
2022-01-10 00:10:09 +0100

    Update: Docking indicator for splitters basically working. Todo: Needs a fix for the resize problem when a new splitter is added.

---
+++ samples/docking/docking/dockingevent.cpp
@@ -0,0 +1,22 @@
+
+#if wxUSE_DOCKING
+
+#include <wx/docking/dockingevent.h>
+
+#if wxUSE_GUI
+wxIMPLEMENT_DYNAMIC_CLASS(wxDockingEvent, wxEvent);
+#endif // wxUSE_GUI
+
+wxDockingEvent::wxDockingEvent(void)
+{
+}
+
+void wxDockingEvent::Assign(const wxDockingEvent &event)
+{
+	wxEvent::operator=(event);
+
+	m_src = event.m_src;
+	m_tgt = event.m_tgt;
+}
+
+#endif // wxUSE_DOCKING
