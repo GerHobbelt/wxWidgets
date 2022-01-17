6649f16df0933a8f5072eaddf6bb59de4e0ec945
Ger Hobbelt <ger@hobbelt.com>
2022-01-18 00:32:10 +0100

    fix: turns out one cannot compile wxWidgets with wxUSE_MENUS=1 when wxUSE_MENUBAR hasn't been turned on as well. Add check+report to chkconf.h for this.

--- include/wx/chkconf.h
+++ include/wx/chkconf.h
@@ -944,6 +944,14 @@
 #   ifdef wxABORT_ON_CONFIG_ERROR
 #       error "wxUSE_MENUS must be defined, please read comment near the top of this file."
 #   else
+#		if !wxUSE_MENUBAR
+#			ifdef wxABORT_ON_CONFIG_ERROR
+#				error "wxUSE_MENUS requires wxUSE_MENUBAR to be turned on as well."
+#			else
+#				undef wxUSE_MENUBAR 
+#				define wxUSE_MENUBAR 1
+#			endif
+#		endif /* !defined(wxUSE_MENUBAR) */
 #       define wxUSE_MENUS 0
 #   endif
 #endif /* !defined(wxUSE_MENUS) */
