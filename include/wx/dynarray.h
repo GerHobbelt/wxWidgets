GerHobbelt/wxWidgets

commit fd0502fa0c87bb1ad8fde25b2a18568a558b86be

add assertion to make sure everybody is using the 'optimized' Add/insert methods of the dynamic array class hierarchy, which were added in commit SHA-1: 93781f2c67d67d6b286d525c926cb34edc9c2a5c as part of the diagnostic/debug action happening. (That WTF still goes unexplained, though.)

include/wx/dynarray.h
@@ -166,6 +166,7 @@ public:
 
     void Add(T lItem, size_t nInsert)
     {
+		wxASSERT_MSG(nInsert > 1, wxString::Format("wxBaseArray::Add(<item>, n = %u)", (unsigned int)nInsert));
 		this->insert(this->end(), nInsert, lItem);
 	}
 

commit 0a8547a33e85c97dac5c2fb5f7b4f09321ac9185

fix for commit SHA-1: fd0502fa0c87bb1ad8fde25b2a18568a558b86be which caused the static build to b0rk when used with thridparty software, e.g. wxFormBuilder, which has a different header load order some times.

include/wx/dynarray.h
@@ -14,6 +14,7 @@
 #include "wx/defs.h"
 
 #include "wx/vector.h"
+#include "wx/string.h"       // for wxString::Format
 
 /*
   This header defines legacy dynamic arrays and object arrays (i.e. arrays